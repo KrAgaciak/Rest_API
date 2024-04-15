0c5def2055abb7c5f1f82b5d249aadf84d867171
Krzysztof <k.agaciak@gmail.com>
2024-04-15 23:32:06 +0200

    Dodaj pliki projektów.

---
+++ Rest_API/Controllers/Animals.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rest_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class Animals : ControllerBase
+    {
+        private Database db1;
+
+        public Animals() {
+            db1 = Database.GetDatabase();
+        }
+
+        [HttpGet(Name = "GetAnimals")]
+        public IEnumerable<Animal> Get()
+        {
+            return db1.Animals.ToArray();
+        }
+
+        [HttpGet("{Id}")]
+        public Animal GetAnimalById(int animalId)
+        {
+            Animal a = null;
+            foreach (Animal animal in db1.Animals)
+            {
+                if (animal.Id.Equals(animalId)) { a = animal; }
+            }
+            return a;
+        }
+
+        [HttpPut(Name = "PutAnimal")]
+        public Animal Put(string name)
+        {
+            Animal a = new Animal();
+            a.Name = name;
+            db1.Animals.Add(a);
+            return a;
+        }
+
+        [HttpPost(Name = "ChangeAnimalAtributes")] // gdzie się przyjmuje jakie wartości 
+        public Animal Post(string name)
+        {
+            Animal a = new Animal();
+            a.Name = name;
+            db1.Animals.Add(a);
+            return a;
+        }
+
+        [HttpDelete(Name = "DeletAnimal")]
+        public Animal Delete(int animalId)
+        {
+            Animal a = null;
+            foreach (Animal animal in db1.Animals)
+            {
+                if (animal.Id.Equals(animalId)) { 
+                    a = animal;
+                    db1.Animals.Remove(animal);
+                }
+            }
+            return a;
+        }
+
+    }
+}
---
+++ Rest_API/Database.cs
@@ -0,0 +1,33 @@
+﻿namespace Rest_API
+{
+    public class Database
+    {
+        public List<Animal> Animals;
+        public List<Visit> Visits;
+        private static Database db;
+
+        private Database()
+        {
+            Animals = new List<Animal>();
+            Visits = new List<Visit>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Animal a = new Animal();
+                a.Name = "A" + i;
+                a.Id = i;
+                a.FurColor = "Green";
+                a.Weight = i * 50;
+                Animals.Add(a);
+            }
+        }
+        public static Database GetDatabase()
+        {
+            if (db == null)
+            {
+                db = new Database();
+            }
+            return db;
+        }
+    }
+}
